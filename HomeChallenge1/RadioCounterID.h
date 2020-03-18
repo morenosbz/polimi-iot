e98b1b21cadbf48a7fecd1fe04590c82ae3d8fff
Seb <morenosbz@gmail.com>
2020-03-18 23:18:34 +0100

    Update filenames

---
+++ HomeChallenge1/RadioCounterID.h
@@ -0,0 +1,19 @@
+/**
+*	Politecnico di Milano
+*	IoT - Home Challenge 1
+*	Message structure
+*/
+
+#ifndef RADIO_COUNTER_ID_H
+#define RADIO_COUNTER_ID_H
+
+typedef nx_struct radio_count_msg {
+  nx_uint16_t counter;		// Counter value
+} radio_count_msg_t;
+
+enum {
+  AM_RADIO_COUNT_MSG = 6,
+  TIMER_PERIOD_MILLI = 250		// TODO Check this value
+};
+
+#endif
