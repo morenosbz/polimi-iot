839c941541656abf22491045159635c0f0e3b0af
Seb <morenosbz@gmail.com>
2020-05-16 17:08:22 +0200

    Question 4

--- HomeChallenge3/filters.py
+++ HomeChallenge3/filters.py
@@ -16,15 +16,43 @@ def question2():
 	print "Packet No. 6949 token " + msgToken
 	f2 = lambda r: CoAP in r and r[CoAP].token == msgToken
 	print "Packets with the same token"
-	my_print = lambda r: r.show()
+	my_print = lambda r: "Packet IP.id= "+ str(r[IP].id)
 
 	hw.nsummary(lfilter=f2, prn=my_print)
 	
 def question3():
 	f3 = lambda r: CoAP in r and r[CoAP].code == 69 and r[IP].dst == "127.0.0.1"
 	my_print = lambda r: "Message ID= "+ str(r[CoAP].msg_id)
+	#my_print = lambda r: r[CoAP].show()
 	h1 = hw.filter(f3)
 	h1.show(my_print)
 	print "Total packets= " + str(len(h1))
 	
-question3()
+def question4():
+		
+	f41 = lambda r: MQTT in r and r[MQTT].type == 1 and r[MQTT].username == "jane"
+	conn_print = lambda r: r[IP].dst + ':' + str(r[TCP].sport)
+	connect_pkts = hw.filter(f41)
+	#connect_seq = map(my_print, connect_pkts)
+	print "Connections with username = jane"
+	connect_pkts.show(conn_print)
+	connect_ports = map(lambda r: r[TCP].sport, connect_pkts)
+	
+	f4 = lambda r: MQTT in r and r[MQTT].type == 3 and "factory/department" in r[MQTT].topic
+	topic_pkts = hw.filter(f4)
+	print "Packets with the factory/department topic = " + str(len(topic_pkts))
+	
+	f42 = lambda r: r[TCP].sport in connect_ports
+	jane_publish = topic_pkts.filter(f42)
+	print "Published messages from jane"
+	my_print = lambda r: "Packet IP.id= "+ str(r[IP].id)
+	jane_publish.show(my_print)
+	print "Total packets from a connection with username eq jane = " + str(len(jane_publish))
+	
+question4()
+
+
+
+
+
+
