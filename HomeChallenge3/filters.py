5d7f91f48247475abb245aea29bda2d507657965
Seb <morenosbz@gmail.com>
2020-05-16 17:36:11 +0200

    DNS search

--- HomeChallenge3/filters.py
+++ HomeChallenge3/filters.py
@@ -31,25 +31,30 @@ def question3():
 def question4():
 		
 	f41 = lambda r: MQTT in r and r[MQTT].type == 1 and r[MQTT].username == "jane"
-	conn_print = lambda r: r[IP].dst + ':' + str(r[TCP].sport)
 	connect_pkts = hw.filter(f41)
 	#connect_seq = map(my_print, connect_pkts)
 	print "Connections with username = jane"
+	conn_print = lambda r: r[IP].src + ':' + str(r[TCP].sport)
 	connect_pkts.show(conn_print)
 	connect_ports = map(lambda r: r[TCP].sport, connect_pkts)
 	
-	f4 = lambda r: MQTT in r and r[MQTT].type == 3 and "factory/department" in r[MQTT].topic
-	topic_pkts = hw.filter(f4)
+	f42 = lambda r: MQTT in r and r[MQTT].type == 3 and "factory/department" in r[MQTT].topic
+	topic_pkts = hw.filter(f42)
 	print "Packets with the factory/department topic = " + str(len(topic_pkts))
 	
-	f42 = lambda r: r[TCP].sport in connect_ports
-	jane_publish = topic_pkts.filter(f42)
+	f43 = lambda r: r[TCP].sport in connect_ports
+	jane_publish = topic_pkts.filter(f43)
 	print "Published messages from jane"
 	my_print = lambda r: "Packet IP.id= "+ str(r[IP].id)
 	jane_publish.show(my_print)
 	print "Total packets from a connection with username eq jane = " + str(len(jane_publish))
 	
-question4()
+def question5():
+	f5 = lambda r: DNS in r
+	my_print = lambda r: r[DNS].show()
+	hw.nsummary(lfilter=f5, prn=my_print)
+		
+question5()
 
 
 
